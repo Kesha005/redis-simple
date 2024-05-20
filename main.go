29d085ed537ff1f78b2b4b858508fe07712d2b64
Kesha005 <backendtm005@gmail.com>
2024-05-20 17:02:28 +0500

    formatted min.go file

--- main.go
+++ main.go
@@ -1,77 +1,64 @@
-package main 
-
+package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 	redis "github.com/go-redis/redis/v8"
 )
 
-
-
 var ctx = context.Background()
 
-func RedisSetter(redcl redis.Client,key string, value interface{})error{
+func RedisSetter(redcl redis.Client, key string, value interface{}) error {
 
-	err := redcl.Set(ctx, key , value, 0).Err()
-	if err!=nil{
+	err := redcl.Set(ctx, key, value, 0).Err()
+	if err != nil {
 		fmt.Println("Error on setting chache ", err)
-		return err 
+		return err
 	}
 	return nil
-	
 
 }
 
-
-
-func RedisGetter(redcl redis.Client, key string,)(interface{}, error){
-	val, err := redcl.Get(ctx,key).Result()
-	if err!=nil{
-		if err==redis.Nil{
+func RedisGetter(redcl redis.Client, key string) (interface{}, error) {
+	val, err := redcl.Get(ctx, key).Result()
+	if err != nil {
+		if err == redis.Nil {
 			fmt.Println("this key does not excists")
-			return nil,err
+			return nil, err
 		}
-		return nil,err
+		return nil, err
 	}
 
 	return val, nil
 }
 
-
-func main(){
+func main() {
 	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     "localhost:6379",
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 
-	
-
-	pong ,err := client.Ping(context.Background()).Result()
-	if err!=nil{
-		fmt.Println("There are some error like ",err)
-		return 
+	pong, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Println("There are some error like ", err)
+		return
 	}
 
-	fmt.Println("Redis server worked all is ok ",pong)
+	fmt.Println("Redis server worked all is ok ", pong)
 
-	serr :=RedisSetter(*client ,"name","Kesha") 
+	serr := RedisSetter(*client, "name", "Kesha")
 
-	if serr!=nil{
+	if serr != nil {
 		panic(serr)
-		return 
+		return
 	}
 
-	val,ver:= RedisGetter(*client, "name");
-	if ver!=nil{
+	val, ver := RedisGetter(*client, "name")
+	if ver != nil {
 		fmt.Println(ver)
-		return 
+		return
 	}
 	fmt.Println("the value is ", val)
-
-
-
-
 
 }
